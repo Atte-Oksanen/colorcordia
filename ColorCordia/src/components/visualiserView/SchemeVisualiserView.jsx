7328ac8cfb38969823785805eb3d427adac0f3ef
Atte-Oksanen <atteoksanen@outlook.com>
2023-10-30 18:48:18 +0200

    finalised scheme viewer functionality

--- ColorCordia/src/components/visualiserView/SchemeVisualiserView.jsx
+++ ColorCordia/src/components/visualiserView/SchemeVisualiserView.jsx
@@ -5,9 +5,18 @@ import VisualiserCalendar from "./VisualiserCalendar"
 import VisualiserDashboard from "./VisualiserDashboard"
 import { getPalettes } from "../../services/palettes"
 import PaletteDropDown from "../utils/PaletteDropDown"
+import { createRandomHarmony, randomizeColor } from "../../utils/colorRandomizer"
+import { getTextColor } from "../../utils/colorConverters"
 
 const SchemeVisualiserView = ({ palettes, setPalettes, user }) => {
   const [colors, setColors] = useState([])
+
+  const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    gridTemplateRows: '1fr',
+    height: '1rem'
+  }
   useEffect(() => {
     if (palettes.length < 1) {
       (async () => {
@@ -15,6 +24,15 @@ const SchemeVisualiserView = ({ palettes, setPalettes, user }) => {
       })()
     }
   }, [])
+
+  useEffect(() => {
+    setColors(createRandomHarmony(randomizeColor()))
+  }, [])
+
+  const randomizeColors = () => {
+    const randColors = createRandomHarmony(randomizeColor())
+    setColors(randColors)
+  }
 
   const handleColorInput = event => {
     event.preventDefault()
@@ -25,6 +43,9 @@ const SchemeVisualiserView = ({ palettes, setPalettes, user }) => {
     setColors(colors)
   }
 
+  if (colors.length < 1) {
+    return null
+  }
   return (
     <div>
       <form onSubmit={handleColorInput}>
@@ -43,13 +64,26 @@ const SchemeVisualiserView = ({ palettes, setPalettes, user }) => {
           <PaletteDropDown palettes={palettes.filter(palette => palette.user.id === user.id)} setColors={setColors}></PaletteDropDown>
         </div>
       }
-      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '40rem', overflow: 'hidden' }}>
+      <button onClick={() => randomizeColors()}>Randomize palette</button>
+      <br />
+      <div>
+        Shown palette
+        <div style={{ ...gridContainerStyle, width: '30rem' }}>
+          <div style={{ background: colors[0], gridArea: '1/1/2/2', color: getTextColor(colors[0]) }}>{colors[0]}</div>
+          <div style={{ background: colors[1], gridArea: '1/2/2/3', color: getTextColor(colors[1]) }}>{colors[1]}</div>
+          <div style={{ background: colors[2], gridArea: '1/3/2/4', color: getTextColor(colors[2]) }}>{colors[2]}</div>
+          <div style={{ background: colors[3], gridArea: '1/4/2/5', color: getTextColor(colors[3]) }}>{colors[3]}</div>
+          <div style={{ background: colors[4], gridArea: '1/5/2/6', color: getTextColor(colors[4]) }}>{colors[4]}</div>
+        </div>
+      </div>
+      <br />
+      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '38rem', overflow: 'hidden', transform: 'scale(0.9)' }}>
         <VisualiserBlog colors={colors}></VisualiserBlog>
       </div>
-      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '40rem', overflow: 'hidden', marginLeft: '1rem' }}>
+      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '38rem', overflow: 'hidden', marginLeft: '0.5rem', transform: 'scale(0.9)' }}>
         <VisualiserCalendar colors={colors}></VisualiserCalendar>
       </div>
-      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '40rem', overflow: 'hidden' }}>
+      <div style={{ display: 'inline-block', borderRadius: '1rem', width: '38rem', overflow: 'hidden', transform: 'scale(0.9)' }}>
         <VisualiserDashboard colors={colors}></VisualiserDashboard>
       </div>
     </div>
--- ColorCordia/src/utils/colorRandomizer.js
+++ ColorCordia/src/utils/colorRandomizer.js
@@ -1,10 +1,11 @@
 import { hsvToRgb, rgbToHex } from "./colorConverters"
+import { analogousHarmony, complementaryHarmony, compoundHarmony, doubleSplitComplementary, monoHarmony, shadeHarmony, splitComplementaryHarmony, squareHarmony, triadHarmony } from "./colorHarmonies"
 
 export const randomizeColor = () => {
   return rgbToHex({
-    r: Math.random() * 245 + 10,
-    g: Math.random() * 245 + 10,
-    b: Math.random() * 245 + 10
+    r: Math.floor(Math.random() * 245 + 10),
+    g: Math.floor(Math.random() * 245 + 10),
+    b: Math.floor(Math.random() * 245 + 10)
   })
 }
 
@@ -14,4 +15,29 @@ export const randomizeColorWheelPos = () => {
     s: (Math.random() + 0.1) % 0.98,
     v: 1
   }))
+}
+
+export const createRandomHarmony = hex => {
+  const harmonyNum = Math.round(Math.random() * 9)
+  console.log(hex)
+  switch (harmonyNum) {
+    case 1:
+      return analogousHarmony(hex)
+    case 2:
+      return monoHarmony(hex)
+    case 3:
+      return triadHarmony(hex)
+    case 4:
+      return complementaryHarmony(hex)
+    case 5:
+      return splitComplementaryHarmony(hex)
+    case 6:
+      return doubleSplitComplementary(hex)
+    case 7:
+      return squareHarmony(hex)
+    case 8:
+      return compoundHarmony(hex)
+    default:
+      return shadeHarmony(hex)
+  }
 }

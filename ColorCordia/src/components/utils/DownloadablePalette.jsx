fc0a012f16abcfbf0eb5c5646d8f284b8cf9379a
Atte Oksanen <atteoksanen@outlook.com>
2023-11-11 13:14:46 +0200

    optimized downloadable palette

--- ColorCordia/src/components/utils/DownloadablePalette.jsx
+++ ColorCordia/src/components/utils/DownloadablePalette.jsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react"
 import DownloadIcon from "../icons/DownloadIcon"
 
 const DownloadablePalette = ({ palette, type }) => {
-  const [dataUrl, setDataUrl] = useState(null)
-  useEffect(() => {
+
+  const downloadImage = () => {
     const canvas = document.createElement('canvas')
     canvas.width = 1500
     canvas.height = 1080
@@ -13,25 +13,21 @@ const DownloadablePalette = ({ palette, type }) => {
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.fillStyle = 'black'
     for (let index = 0, x = 0; index < palette.length; index++, x = x + 300) {
-      ctx.font = 'bold 38px Arial'
+      ctx.font = '38px Rubik'
       ctx.fillStyle = palette[index].hex
       ctx.fillRect(x, 0, 300, 850)
       ctx.fillStyle = 'black'
       ctx.fillText(`${palette[index].hex}`, x + 15, 900)
-      ctx.font = '25px Arial'
+      ctx.font = 'lighter 25px Rubik'
       ctx.fillText(`${palette[index].name}`, x + 15, 950)
     }
-    ctx.font = 'Bold 30px Arial'
+    ctx.font = '30px Rubik'
     ctx.fillText("ColorCordia*", 15, 1060)
     const text = `${type} pallette from ${palette[2].hex}`
     ctx.fillText(text, 15, 1010)
-    setDataUrl(canvas.toDataURL())
-  }, [palette, setDataUrl, type])
-
-  const downloadImage = () => {
     const link = document.createElement('a')
     link.download = `${type}-${palette.map(element => element.hex).toString().replaceAll(',', '-')}.png`
-    link.href = dataUrl
+    link.href = canvas.toDataURL()
     link.click()
   }
 

46a94fda9a3f3e30ede5fa6512ea8ef3772ce1f2
Atte-Oksanen <atteoksanen@outlook.com>
2023-11-13 13:54:04 +0200

    updated mobile UI

--- ColorCordia/src/components/paletteView/PaletteView.jsx
+++ ColorCordia/src/components/paletteView/PaletteView.jsx
@@ -5,7 +5,7 @@ import PaletteGenerator from "./paletteGenerator"
 const PaletteView = ({ color, setColor }) => {
   return (
     <div className="md:grid md:grid-cols-[1fr_4fr]">
-      <div className="p-4 md:my-4 my-2 border-r-2  border-gray-200">
+      <div className="p-4 md:my-4 my-2 md:border-r-2  md:border-gray-200">
         <ColorTweaker color={color} setColor={setColor}></ColorTweaker>
       </div>
       <div className="md:h-screen overflow-y-auto">

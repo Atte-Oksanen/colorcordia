9857460eb19a8091c81c1cae0d920e7bd5e08e1c
Atte Oksanen <atteoksanen@outlook.com>
2023-11-13 20:43:02 +0200

    fixed node compability issues

--- ColorCordia_backend/utils/checkProfanity.js
+++ ColorCordia_backend/utils/checkProfanity.js
@@ -1,25 +1,22 @@
 const badWords = require('naughty-words')
 
 const checkProfanity = (input) => {
-
   const cleanedInput = input.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
-    .replaceAll('4', 'a')
-    .replaceAll('8', 'b')
-    .replaceAll('3', 'e')
-    .replaceAll('6', 'g')
-    .replaceAll('0', 'o')
-    .replaceAll('9', 'p')
-    .replaceAll('5', 's')
-    .replaceAll('7', 't')
-    .replaceAll('2', 'z')
-
+    .replace(/4/g, 'a')
+    .replace(/8/g, 'b')
+    .replace(/3/g, 'e')
+    .replace(/6/g, 'g')
+    .replace(/0/g, 'o')
+    .replace(/9/g, 'p')
+    .replace(/5/g, 's')
+    .replace(/7/g, 't')
+    .replace(/2/g, 'z')
   const keys = Object.keys(badWords)
   for (let index = 0; index < keys.length; index++) {
     if (badWords[keys[index]].some(word => cleanedInput.replaceAll('1', 'i').includes(word)) || badWords[keys[index]].some(word => cleanedInput.replaceAll('1', 'l').includes(word))) {
       return true
     }
   }
-
   return false
 }
 

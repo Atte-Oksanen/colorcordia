41adf5d033f1d7e17313d2fb9d9d751b2add0748
Atte-Oksanen <atteoksanen@outlook.com>
2023-11-14 21:57:34 +0200

    added different color spaces

--- ColorCordia/src/components/utils/ColorPaletteSkeleton.jsx
+++ ColorCordia/src/components/utils/ColorPaletteSkeleton.jsx
@@ -1,17 +1,47 @@
-/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { hexToRgb, rgbToHsv } from "../../utils/colorConverters"
+
 const ColorPaletteSkeleton = (props) => {
+  const [shownColorSpace, setColorSpace] = useState('Hex')
+
+  const getColorCode = hex => {
+    switch (shownColorSpace) {
+      case 'Hex':
+        return hex
+      case 'HSV': {
+        const hsv = rgbToHsv(hexToRgb(hex))
+        return `${Math.round(hsv.h * 360)}°, ${Math.round(hsv.s * 100)}%, ${Math.round(hsv.v * 100)}%`
+      }
+      case 'RGB': {
+        const rgb = hexToRgb(hex)
+        return `${rgb.r}, ${rgb.g}, ${rgb.b}`
+      }
+    }
+  }
   return (
     <div className="flex items-center justify-center md:h-screen mx-4">
       <div className="md:w-[95%] my-4 h-fit">
-        <h2 className="text-2xl font-normal my-2">
-          {`${props.type} pallette from ${props.colors[2].hex}`}
-        </h2>
+        <div className="md:flex justify-between">
+          <h2 className="text-2xl font-normal my-2">
+            {`${props.type} pallette from ${props.colors[2].hex}`}
+          </h2>
+          <div>
+            <label htmlFor="shownUnit">
+              Shown color system
+            </label>
+            <select id="shownUnit" className="dropdown mx-4 my-2" value={shownColorSpace} onChange={event => setColorSpace(event.target.value)}>
+              <option>Hex</option>
+              <option>RGB</option>
+              <option>HSV</option>
+            </select>
+          </div>
+        </div>
         <div className="md:grid hidden grid-rows-[10fr_1fr] h-[60vh]">
           <div className="grid grid-cols-5 rounded-md overflow-hidden border border-gray-300">
             {props.colors.map(color => <div key={Math.random()} style={{ background: color.hex }}></div>)}
           </div>
           <div className="grid grid-cols-5 h-fit">
-            {props.colors.map(color => <div className="px-5 py-2 text-center" key={Math.random()}>{color.hex} - {color.name}</div>)}
+            {props.colors.map(color => <div className="px-5 py-2 text-center" key={Math.random()}>{getColorCode(color.hex)} - {color.name}</div>)}
           </div>
         </div>
         <div>
@@ -20,7 +50,7 @@ const ColorPaletteSkeleton = (props) => {
               return (
                 <div className="grid grid-rows-[2fr_1fr] grid-cols-1" key={Math.random()}>
                   <div className="rounded-lg border border-gray-300" key={Math.random()} style={{ background: color.hex }}></div>
-                  <div className="py-2 text-center" key={Math.random()}>{color.hex} - {color.name}</div>
+                  <div className="py-2 text-center" key={Math.random()}>{getColorCode(color.hex)} - {color.name}</div>
                 </div>
               )
             })}
